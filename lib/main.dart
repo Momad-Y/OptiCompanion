625175dafa898287276ffad41706d0d2a1bf1139
Mohamed Youssef <Mohamed.y.abdelnasser@gmail.com>
2024-05-05 19:37:33 +0300

    Disable debug mode banner in MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: colorTheme(context),
       home: const HomePage(),
     );
