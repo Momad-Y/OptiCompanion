16404404bb50e7942741eca5fb0e5a0477d52891
Mohamed Youssef <Mohamed.y.abdelnasser@gmail.com>
2024-05-05 18:24:40 +0300

    Refactor home page layout

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -36,55 +36,147 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        onHorizontalDragEnd: (details) {
-          if (details.primaryVelocity! < -_velocityThreshold) {
-            _decrementCounter();
-          } else if (details.primaryVelocity! > _velocityThreshold) {
-            _incrementCounter();
-          }
-        },
-        child: Scaffold(
-            body: Center(
-          child: GridView.count(
-            primary: false,
-            shrinkWrap: true,
-            padding: const EdgeInsets.all(20),
-            crossAxisSpacing: 50,
-            mainAxisSpacing: 50,
-            crossAxisCount: 2,
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.all(8),
-                color: Colors.teal[100],
-                child: const Text("He'd have you all unravel at the"),
+      onHorizontalDragEnd: (details) {
+        if (details.primaryVelocity! < -_velocityThreshold) {
+          _decrementCounter();
+        } else if (details.primaryVelocity! > _velocityThreshold) {
+          _incrementCounter();
+        }
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 30),
+              Image.asset('assets/images/App Logo.png', width: 65, height: 65),
+              const SizedBox(height: 20),
+              Text(
+                'Home Screen',
+                style: Theme.of(context).textTheme.titleLarge!,
+                textAlign: TextAlign.center,
               ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                color: Colors.teal[200],
-                child: const Text('Heed not the rabble'),
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                color: Colors.teal[300],
-                child: const Text('Sound of screams but the'),
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                color: Colors.teal[400],
-                child: const Text('Who scream'),
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                color: Colors.teal[500],
-                child: const Text('Revolution is coming...'),
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                color: Colors.teal[600],
-                child: const Text('Revolution, they...'),
+              const SizedBox(height: 70),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 150,
+                            height: 150,
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25),
+                              border: Border.all(width: 4),
+                            ),
+                            child: const Text(""),
+                          ),
+                          const SizedBox(width: 45),
+                          Container(
+                            width: 150,
+                            height: 150,
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25),
+                              border: Border.all(width: 4),
+                            ),
+                            child: const Text(""),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 40),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 150,
+                            height: 150,
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25),
+                              border: Border.all(width: 4),
+                            ),
+                            child: const Text(""),
+                          ),
+                          const SizedBox(width: 45),
+                          Container(
+                            width: 150,
+                            height: 150,
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25),
+                              border: Border.all(width: 4),
+                            ),
+                            child: const Text(""),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 40),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 150,
+                            height: 150,
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25),
+                              border: Border.all(width: 4),
+                            ),
+                            child: const Text(""),
+                          ),
+                          const SizedBox(width: 45),
+                          Container(
+                            width: 150,
+                            height: 150,
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25),
+                              border: Border.all(width: 4),
+                            ),
+                            child: const Text(""),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 40),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                            width: 150,
+                            height: 150,
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25),
+                              border: Border.all(width: 4),
+                            ),
+                            child: const Text(""),
+                          ),
+                          Container(
+                            width: 150,
+                            height: 150,
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25),
+                              border: Border.all(width: 4),
+                            ),
+                            child: const Text(""),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 80),
+                    ],
+                  ),
+                ),
               ),
             ],
           ),
-        )));
+        ),
+      ),
+    );
   }
 }
