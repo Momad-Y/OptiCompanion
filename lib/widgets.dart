95576ce7846c4fb1327c30deb9fbe6036becc26d
Mohamed Youssef <Mohamed.y.abdelnasser@gmail.com>
2024-05-05 19:36:24 +0300

    Add selectedHomeContainer widget

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -13,6 +13,19 @@ homeContainer(BuildContext context, Widget child) {
   );
 }
 
+selectedHomeContainer(BuildContext context, Widget child) {
+  return Container(
+    width: 150,
+    height: 150,
+    padding: const EdgeInsets.all(8),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(25),
+      border: Border.all(width: 4, color: Theme.of(context).colorScheme.outline),
+    ),
+    child: child,
+  );
+}
+
 appLogo(BuildContext context) {
   String logoPath = MediaQuery.of(context).platformBrightness == Brightness.light
       ? 'assets/images/OptiCompanion-Transparent - Light.png'
