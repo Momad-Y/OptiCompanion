480e7f682e46e75c8131b17049a94892faf19571
Mohamed Youssef <Mohamed.y.abdelnasser@gmail.com>
2024-05-05 18:24:36 +0300

    Update theme and text styles in App widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import './pages/pages.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
@@ -11,6 +11,16 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+            titleLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.w900, color: Colors.black, fontFamily: 'Arial')),
+        colorScheme: const ColorScheme.light(
+            primary: Color(0xFFF0F0F0),
+            secondary: Color(0xFF0F0F0F),
+            tertiary: Color(0xFF090c9b),
+            outline: Color(0xFF5AF705)),
+        useMaterial3: true,
+      ),
       home: const HomePage(),
     );
   }
