a9af92bb0c14a3c2d0e5f550c4331be7a7ec20cb
Mohamed Youssef <Mohamed.y.abdelnasser@gmail.com>
2024-05-06 04:41:43 +0300

    Update appLogo and nextPageButton functions

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './themes.dart';
 
 homeContainer(BuildContext context, Widget child) {
   return Container(
@@ -26,12 +27,12 @@ selectedHomeContainer(BuildContext context, Widget child) {
   );
 }
 
-appLogo(BuildContext context) {
+appLogo(BuildContext context, double width, double height) {
   String logoPath = MediaQuery.of(context).platformBrightness == Brightness.light
       ? 'assets/images/OptiCompanion-Transparent - Light.png'
       : 'assets/images/OptiCompanion-Transparent - Dark.png';
 
-  return Image.asset(logoPath, width: 90, height: 90);
+  return Image.asset(logoPath, width: width, height: height);
 }
 
 icon(BuildContext context, String path) {
@@ -42,4 +43,56 @@ icon(BuildContext context, String path) {
       : logoPath = '${logoPath.split('.').first} - dark.png';
 
   return Image.asset(logoPath, width: 70, height: 70);
+}
+
+nextPageButton(BuildContext context, String route) {
+  return ElevatedButton(
+    onLongPress: () => Navigator.pushNamed(context, route),
+    onPressed: () {},
+    child: Container(
+      width: 220,
+      height: 60,
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        border: Border.all(width: 4, color: Theme.of(context).colorScheme.tertiary),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // const SizedBox(width: 10),
+          Text('Next', style: textTheme(context).labelLarge),
+          const SizedBox(width: 10),
+          Icon(Icons.arrow_forward_rounded, color: Theme.of(context).colorScheme.secondary, size: 35),
+        ],
+      ),
+    ),
+  );
+}
+
+selectedNextPageButton(BuildContext context, String route) {
+  return ElevatedButton(
+    onLongPress: () => Navigator.pushNamed(context, route),
+    onPressed: () {},
+    child: Container(
+      width: 220,
+      height: 60,
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        border: Border.all(width: 4, color: Theme.of(context).colorScheme.outline),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // const SizedBox(width: 10),
+          Text('Next', style: textTheme(context).labelLarge),
+          const SizedBox(width: 10),
+          Icon(Icons.arrow_forward_rounded, color: Theme.of(context).colorScheme.secondary, size: 35),
+        ],
+      ),
+    ),
+  );
 }
