14fa721df7a91e26a7110a1411642e70f0cf75fe
Mohamed Youssef <Mohamed.y.abdelnasser@gmail.com>
2024-05-06 21:56:56 +0300

    Add theme settings page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './pages/pages.dart';
 import './themes.dart';
+import './settings.dart';
 
 void main() {
   runApp(const App());
@@ -15,12 +16,19 @@ class App extends StatelessWidget {
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: colorTheme(context),
+      theme: lightTheme(context),
+      darkTheme: darkTheme(context),
+      themeMode: AppSettings(context).getTheme == 0
+          ? ThemeMode.system
+          : AppSettings(context).getTheme == 1
+              ? ThemeMode.light
+              : ThemeMode.dark,
       initialRoute: '/welcome1',
       routes: {
         '/home': (context) => const HomePage(),
         '/welcome1': (context) => const WelcomePage1(),
         '/welcome2': (context) => const WelcomePage2(),
+        '/welcome_settings1': (context) => const WelcomeSettingsPage1(),
       },
     );
   }
