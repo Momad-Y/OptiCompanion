742e8c8ed1a53d4e608affe3023018f292f537c8
Mohamed Youssef <Mohamed.y.abdelnasser@gmail.com>
2024-05-07 04:15:00 +0300

    Refactor Tts class to use variables for language, speed, volume, and gender

--- lib/tts.dart
+++ lib/tts.dart
@@ -1,15 +1,22 @@
 import 'package:flutter_tts/flutter_tts.dart';
 
+Tts mainTts = Tts();
+
 class Tts {
+  String _language = "Arabic";
+  int _speed = 3;
+  int _volume = 10;
+  String _gender = "Female";
+
   FlutterTts initTts(FlutterTts? flutterTts) {
     flutterTts = FlutterTts();
 
     _setAwaitOptions(flutterTts);
 
-    setTtsLanguage(flutterTts, "en");
+    setTtsLanguage(flutterTts, _language);
     setTtsSpeed(flutterTts, 3);
     setTtsVolume(flutterTts, 10);
-    setTtsGender(flutterTts, "female");
+    setTtsGender(flutterTts, _gender);
 
     return flutterTts;
   }
@@ -19,10 +26,12 @@ class Tts {
   }
 
   FlutterTts setTtsLanguage(FlutterTts flutterTts, String language) {
-    if (language == "en") {
+    if (language == "English") {
       language = "en-GB";
-    } else if (language == "ar") {
+      _language = "English";
+    } else if (language == "Arabic") {
       language = "ar";
+      _language = "Arabic";
     }
 
     flutterTts.setLanguage(language);
@@ -31,15 +40,18 @@ class Tts {
   }
 
   Future<void> setTtsGender(FlutterTts flutterTts, String gender) async {
-    if (gender == "male") {
+    if (gender == "Male") {
+      _gender = "Male";
       await flutterTts.setPitch(0.5);
-    } else if (gender == "female") {
+    } else if (gender == "Female") {
+      _gender = "Female";
       await flutterTts.setPitch(1.0);
     }
   }
 
   Future<void> setTtsSpeed(FlutterTts flutterTts, int intSpeed) async {
     intSpeed = intSpeed * 2;
+    _speed = intSpeed;
     double speed = intSpeed / 10;
 
     await flutterTts.setSpeechRate(speed);
@@ -47,6 +59,12 @@ class Tts {
 
   Future<void> setTtsVolume(FlutterTts flutterTts, int intVolume) async {
     double volume = intVolume / 10;
+    _volume = intVolume;
     await flutterTts.setVolume(volume);
   }
+
+  get getLanguage => _language;
+  get getSpeed => _speed ~/ 2;
+  get getVolume => _volume;
+  get getGender => _gender;
 }
