7823cef5581758154608203075e507c496d8c0d5
Mohamed Youssef <Mohamed.y.abdelnasser@gmail.com>
2024-05-09 06:22:38 +0300

    Add new pages to the project

--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -3,3 +3,5 @@ export './welcome_page_1.dart';
 export './welcome_page_2.dart';
 export './welcome_settings_page_1.dart';
 export './welcome_settings_page_2.dart';
+export './document_reader_page.dart';
+export './settings_page_1.dart';
