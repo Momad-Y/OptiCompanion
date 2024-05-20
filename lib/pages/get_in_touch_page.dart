df5cbe8622cadb50510db2fa46d74a7a1aea2c93
Mohamed Youssef <Mohamed.y.abdelnasser@gmail.com>
2024-05-20 20:38:37 +0300

    Update button width and padding in GetInTouchPage

--- lib/pages/get_in_touch_page.dart
+++ lib/pages/get_in_touch_page.dart
@@ -142,7 +142,8 @@ class _GetInTouchPageState extends State<GetInTouchPage> {
                         _speak();
                       },
                       child: Container(
-                        padding: const EdgeInsets.fromLTRB(15, 0, 15, 5),
+                        width: tts!.getLanguage == "English" ? 280 : 190,
+                        padding: const EdgeInsets.fromLTRB(15, 0, 15, 7),
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(20),
                           border: Border.all(
