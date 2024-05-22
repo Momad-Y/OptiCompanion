88893f702c97bcb17b64c4eea6be8bc6c7c13ce2
Mohamed Youssef <Mohamed.y.abdelnasser@gmail.com>
2024-05-22 22:23:40 +0300

    Update TTS initialization in initState()

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -57,7 +57,7 @@ class _HomePageState extends State<HomePage> {
   initState() {
     super.initState();
     tts = mainTts;
-    flutterTts = tts!.initTts(flutterTts);
+    flutterTts = tts!.initTts(flutterTts, false);
     _pageText = tts!.getLanguage == "English" ? _pageTextEn : _pageTextAr;
     _speak();
   }
